44e10be035d3847c5ccce36f65b04f595652f412
Rocio Fuentes <rocio.fc.511@gmail.com>
2021-07-08 23:06:38 -0500

    Se agregó la conexión  a la base de datos.
    - Se listan las casas de manera dinámica

--- InmobiliariaArrow/InmobiliariaArrow/Startup.cs
+++ InmobiliariaArrow/InmobiliariaArrow/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InmobiliariaArrow.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace InmobiliariaArrow
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("azuredb")));
+
             services.AddControllersWithViews();
         }
 
