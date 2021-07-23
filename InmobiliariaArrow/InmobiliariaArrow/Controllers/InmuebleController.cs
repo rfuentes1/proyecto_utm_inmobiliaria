596bdf865bb25c3cb82bbaaad30b8b0a483daa2c
Adrián Méndez <adrianmendeznieves@gmail.com>
2021-07-22 21:29:45 -0500

    La tabla de inmuebles se carga desde la base de datos.

--- InmobiliariaArrow/InmobiliariaArrow/Controllers/InmuebleController.cs
+++ InmobiliariaArrow/InmobiliariaArrow/Controllers/InmuebleController.cs
@@ -18,20 +18,16 @@ namespace InmobiliariaArrow.Controllers
 
         public IActionResult Index()
         {
-            var inmueble = new List<InmuebleTemp>{
-                new InmuebleTemp{Id = 1, Inmueble = "asdfs",
-                Precio = 2143, Descripcion = "fdsfs", Estatus = "fsaf", Tipo_vivienda = "asff",},
-                new InmuebleTemp{Id = 2, Inmueble = "Terreno",
-                Precio = 2134, Descripcion = "fsdfa", Estatus = "asdf", Tipo_vivienda = "fsdfa",},
-                new InmuebleTemp{Id = 3, Inmueble = "Campo",
-                Precio = 2143, Descripcion = "safsf", Estatus = "asdfsa", Tipo_vivienda = "asdf",},
-                new InmuebleTemp{Id = 4, Inmueble = "Casa",
-                Precio = 4213, Descripcion = "sfsdfsd", Estatus = "asddfsaf", Tipo_vivienda = "adfasdfd",},
-                new InmuebleTemp{Id = 5, Inmueble = "Casa",
-                Precio = 1243, Descripcion = "fasdfaf", Estatus = "fsadfasfd", Tipo_vivienda = "sdfsadf",}
-            };
-            ViewBag.Inmuebles = inmueble;
-            ViewData["inmueble"] = inmueble;
+            var inmueblesDTO = _dbContext.Inmuebles
+            .Select(inmueble => new InmuebleDto
+            {
+                Titulo = inmueble.Titulo,
+                Precio = inmueble.Precio,
+                Operacion = inmueble.Operacion, 
+                Descripcion = inmueble.Descripcion,
+                Superficie = inmueble.Superficie
+            });
+            ViewBag.Inmuebles = inmueblesDTO;
             ViewData["Estilo"] = "inmueble.css";
             return View();
         }
