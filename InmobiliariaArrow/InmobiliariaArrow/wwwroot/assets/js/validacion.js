0589a774cf0e7a23b8980fbb41343e90141fc62c
Rocio Fuentes <rocio.fc.511@gmail.com>
2021-10-27 16:12:58 -0500

    Se agregaron validaciones a formularios de editar y agregar.

---
+++ InmobiliariaArrow/InmobiliariaArrow/wwwroot/assets/js/validacion.js
@@ -0,0 +1,11 @@
+﻿function estaLleno() {
+    let campos = new FormData(document.forms.formulario);
+    for(let campo of campos.keys()){
+        if(!campos.get(campo) || (campo === 'fotos' && campos.get(campo).size<=0)){
+            alert('Todos los campos son obligatorios.');
+            return false;
+        }
+    }
+    return true;
+}
+
