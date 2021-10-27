9b8b2e9a2ddc0abc32dbd678635ae9ec344bd3b8
Rocio Fuentes <rocio.fc.511@gmail.com>
2021-10-27 16:22:48 -0500

    Se agregaron validaciones al form de login.

---
+++ InmobiliariaArrow/InmobiliariaArrow/wwwroot/assets/js/validacionLogin.js
@@ -0,0 +1,11 @@
+﻿function estaLleno() {
+    let campos = new FormData(document.forms.loginForm);
+    for(let campo of campos.keys()){
+        if(!campos.get(campo)){
+            alert('El usuario y password son obligatorios.');
+            return false;
+        }
+    }
+    return true;
+}
+
