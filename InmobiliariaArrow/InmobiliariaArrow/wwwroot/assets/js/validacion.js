c257f76e0d2f5fe95d2d17b898e057de280e5569
Rocio Fuentes <rocio.fc.511@gmail.com>
2021-11-20 19:59:45 -0600

    Se implementó la validación de campos numéricos.

--- InmobiliariaArrow/InmobiliariaArrow/wwwroot/assets/js/validacion.js
+++ InmobiliariaArrow/InmobiliariaArrow/wwwroot/assets/js/validacion.js
@@ -1,4 +1,6 @@
 ﻿function estaLleno() {
+    if(!validarPrecioYSuperficie())
+        return false;
     let campos = new FormData(document.forms.formulario);
     for(let campo of campos.keys()){
         if(!campos.get(campo) || (campo === 'fotos' && campos.get(campo).size<=0)){
@@ -9,3 +11,17 @@
     return true;
 }
 
+function validarPrecioYSuperficie() {
+    let precio = document.getElementById('preciocasa').value;
+    let superficie = document.getElementById('terreno').value;
+    if(isNaN(precio) || precio < 0 )
+    {
+        alert("El campo precio debe ser número positivo.");
+        return false;
+    }
+    if (isNaN(superficie) || superficie < 0 )
+    {
+        alert(" El campo superficie debe ser número positivo.");
+        return false;
+    }
+}
